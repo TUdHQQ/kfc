f079fdc8cf324408b35695a2091dd9a2e5db2ec3
bGlzdGRlcg <listder@outlook.com>
2025-06-27 05:30:05 +0800

    refactor

--- kfc.cpp
+++ kfc.cpp
@@ -1,46 +1,24 @@
 #include "kfc.h"
 
-int main(int argc, char *argv[]){
+int main() {
+  let c = readConfig();
+  auto data = readData(fs::current_path() / c["config"].asString());
 
-    //初始化
-    let cfroot = readJsonFromFile("kfc_config.json");
-    let configFile = cfroot["config"].asString();
-    let infoFile = cfroot["info"].asString();
-    let root = readJsonFromFile(configFile);
-    let aliases = parseFgAlias(infoFile);
+  std::vector<base> bases; // TODO
+  std::vector<diff> diffs;
+  std::map<std::string, fgname> fgnames;
+  std::vector<fgalias> fgaliases;
 
-    //创建一个kif类型的vector用来存储后面的对象
-    let img = parseKIF(root);
+  readSinfo(fs::current_path() / c["sinfo"].asString(), data, bases, diffs,
+            fgnames, fgaliases);
 
-    //创建base，eye，eyebrow，mouth，cheek
-    let base = sortImgForBase(img);
-    let eye_id = getImgItemId(img, 2, "目");
-    let eyebrow_id = getImgItemId(img, 2, "眉");
-    let mouth_id = getImgItemId(img, 2, "口");
-    let cheek_id = getImgItemId(img, 2, "頬");
-    let eye = sortImgById(img, eye_id,"目_");
-    let eyebrow = sortImgById(img, eyebrow_id,"眉_");
-    let mouth = sortImgById(img, mouth_id,"口_");
-    let cheek = sortImgById(img, cheek_id,"頬_");
-
-    //创建output文件夹
-    let outputpath = "output";
-    fs::create_directory(outputpath);
-
-    fs::path temppath = configFile;
-    let perfix = temppath.stem().string();
-
-    //遍历所有需要合成的立绘
-    for(int i = 0; i < base.size(); i++){
-        for (const auto& alias : aliases){
-            let eyebox = getItem(eye, alias.eye);
-            let eyebrowbox = getItem(eyebrow, alias.eyebrow);
-            let mouthbox = getItem(mouth, alias.mouth);
-            let cheekbox = getItem(cheek, alias.cheek);
-            work(base[i], eyebox, eyebrowbox, mouthbox, cheekbox, alias.name, perfix);
-        }
+  for (auto &fg : fgaliases) {
+    for (auto &diff : diffs) {
+      let outpath = fs::current_path() / "output" /
+                    (diff.info.name + "_" + fg.name + ".png");
+      work(outpath, c["base"].asString(), diff, fg);
     }
+  }
 
-    return 0;
-
+  return 0;
 }
